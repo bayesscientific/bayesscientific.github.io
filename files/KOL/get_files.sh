75687f64548ec4e79933b21cf1ae2e3455bc3791
Spencer Woody <spencer.woody@utexas.edu>
2024-11-08 07:14:46 -0800

    Update KOL lectures

---
+++ files/KOL/get_files.sh
@@ -0,0 +1,44 @@
+
+
+
+wget --no-check-certificate -O 2017-08-KOL.DPM_.2017.pdf https://www.bayesianscientific.org/wp-content/uploads/2017/08/KOL.DPM_.2017.pdf
+wget --no-check-certificate -O 2017-10-Larry_Gould_KOL.pdf https://www.bayesianscientific.org/wp-content/uploads/2017/10/Larry_Gould_KOL.pdf
+wget --no-check-certificate -O 2018-01-Irony_BayesKOL_11918.pdf https://www.bayesianscientific.org/wp-content/uploads/2018/01/Irony_BayesKOL_11918.pdf
+wget --no-check-certificate -O 2018-03-Samiran_NI_KOL.pdf https://www.bayesianscientific.org/wp-content/uploads/2018/03/Samiran_NI_KOL.pdf
+wget --no-check-certificate -O 2018-08-Ghosh-KOL-DIA-BSWG-Handouts.pdf https://www.bayesianscientific.org/wp-content/uploads/2018/08/Ghosh-KOL-DIA-BSWG-Handouts.pdf
+wget --no-check-certificate -O 2018-10-NewBayesScreenKOL.pptx https://www.bayesianscientific.org/wp-content/uploads/2018/10/NewBayesScreenKOL.pptx
+wget --no-check-certificate -O 2018-12-BOIN-platform-KOL-YingYuan.pdf https://www.bayesianscientific.org/wp-content/uploads/2018/12/BOIN-platform-KOL-YingYuan.pdf
+wget --no-check-certificate -O 2019-02-casestudieshmpriorsviele.pdf https://www.bayesianscientific.org/wp-content/uploads/2019/02/casestudieshmpriorsviele.pdf
+wget --no-check-certificate -O 2019-03-KOL_FangChen.pdf https://www.bayesianscientific.org/wp-content/uploads/2019/03/KOL_FangChen.pdf
+wget --no-check-certificate -O 2019-07-Nonclinical-Bayesian-projects-AZ.pptx https://www.bayesianscientific.org/wp-content/uploads/2019/07/Nonclinical-Bayesian-projects-AZ.pptx
+wget --no-check-certificate -O 2019-08-DIA-BSWG-KOL_TALouis.pdf https://www.bayesianscientific.org/wp-content/uploads/2019/08/DIA-BSWG-KOL_TALouis.pdf
+wget --no-check-certificate -O 2019-10-HBBR-DIA-BSWG-KOL-v1.1.pdf https://www.bayesianscientific.org/wp-content/uploads/2019/10/HBBR-DIA-BSWG-KOL-v1.1.pdf
+wget --no-check-certificate -O 2019-11-2019_11_22_KOL_MAP.zip https://www.bayesianscientific.org/wp-content/uploads/2019/11/2019_11_22_KOL_MAP.zip
+wget --no-check-certificate -O 2020-08-DIA-KOL-Aug2020.zip https://www.bayesianscientific.org/wp-content/uploads/2020/08/DIA-KOL-Aug2020.zip
+wget --no-check-certificate -O 2020-09-DIAbswgRM.pdf https://www.bayesianscientific.org/wp-content/uploads/2020/09/DIAbswgRM.pdf
+wget --no-check-certificate -O 2020-10-DIA-BSWG-16OCT2020.pdf https://www.bayesianscientific.org/wp-content/uploads/2020/10/DIA-BSWG-16OCT2020.pdf
+wget --no-check-certificate -O 2020-11-DIA-BWG_KOL_Tiwari.2020.pptx https://www.bayesianscientific.org/wp-content/uploads/2020/11/DIA-BWG_KOL_Tiwari.2020.pptx
+wget --no-check-certificate -O 2020-12-DIA_BSWG_KOL_December_2020_Hobbs_Zabor_Kaizer.pdf https://www.bayesianscientific.org/wp-content/uploads/2020/12/DIA_BSWG_KOL_December_2020_Hobbs_Zabor_Kaizer.pdf
+wget --no-check-certificate -O 2021-02-DIA_Hampson_Holzhauer_Feb2021.pptx https://www.bayesianscientific.org/wp-content/uploads/2021/02/DIA_Hampson_Holzhauer_Feb2021.pptx
+wget --no-check-certificate -O 2021-03-Historical-data-borrowing-for-NI-assessment-Quan.pptx https://www.bayesianscientific.org/wp-content/uploads/2021/03/Historical-data-borrowing-for-NI-assessment-Quan.pptx
+wget --no-check-certificate -O 2021-04-Alt_DIApres_Final.pdf https://www.bayesianscientific.org/wp-content/uploads/2021/04/Alt_DIApres_Final.pdf
+wget --no-check-certificate -O 2021-06-BDRIBS-DIA-BSWG-KOL-2021-v2_BW4_SM1.pdf https://www.bayesianscientific.org/wp-content/uploads/2021/06/BDRIBS-DIA-BSWG-KOL-2021-v2_BW4_SM1.pdf
+wget --no-check-certificate -O 2021-08-Payne_KOL_Bayesian_Model_Averaging_of_Longitudinal_Dose_Response_Models.pdf https://www.bayesianscientific.org/wp-content/uploads/2021/08/Payne_KOL_Bayesian_Model_Averaging_of_Longitudinal_Dose_Response_Models.pdf
+wget --no-check-certificate -O 2021-10-Quintana.ALSBayes.10.15.21.pdf https://www.bayesianscientific.org/wp-content/uploads/2021/10/Quintana.ALSBayes.10.15.21.pdf
+wget --no-check-certificate -O 2021-11-Use-of-Bayesian-Hierarchical-Models-in-the-Presentation-of-Subgroup-Analyses.pptx https://www.bayesianscientific.org/wp-content/uploads/2021/11/Use-of-Bayesian-Hierarchical-Models-in-the-Presentation-of-Subgroup-Analyses.pptx
+wget --no-check-certificate -O 2023-03-Schmidli-Thomas-2022-DIA-Bayesian-WG-webinar.pptx https://www.bayesianscientific.org/wp-content/uploads/2023/03/Schmidli-Thomas-2022-DIA-Bayesian-WG-webinar.pptx
+wget --no-check-certificate -O 2022-04-DIA_Bayes-KOL-_INLA-Talk_March-2022_-KrishnaP.pdf https://www.bayesianscientific.org/wp-content/uploads/2022/04/DIA_Bayes-KOL-_INLA-Talk_March-2022_-KrishnaP.pdf
+wget --no-check-certificate -O 2022-04-Phase-1-talk-2022.pdf https://www.bayesianscientific.org/wp-content/uploads/2022/04/Phase-1-talk-2022.pdf
+wget --no-check-certificate -O 2022-05-KOL_Series_May-20-2022.pdf https://www.bayesianscientific.org/wp-content/uploads/2022/05/KOL_Series_May-20-2022.pdf
+wget --no-check-certificate -O 2022-07-Lake_S_BSWG2022_final-1.pdf https://www.bayesianscientific.org/wp-content/uploads/2022/07/Lake_S_BSWG2022_final-1.pdf
+wget --no-check-certificate -O 2022-08-CID_BSWG_AUG2022.pdf https://www.bayesianscientific.org/wp-content/uploads/2022/08/CID_BSWG_AUG2022.pdf
+wget --no-check-certificate -O 2022-07-2022-08-26-FINAL-DIA-Bayesian-Stats-Working-Group.pptx https://www.bayesianscientific.org/wp-content/uploads/2022/07/2022-08-26-FINAL-DIA-Bayesian-Stats-Working-Group.pptx
+wget --no-check-certificate -O 2022-09-Bayesian-KOL-session.pptx.pdf https://www.bayesianscientific.org/wp-content/uploads/2022/09/Bayesian-KOL-session.pptx.pdf
+wget --no-check-certificate -O 2022-10-DIA-BSWG-KOL-CPMP-21OCT2022_final.pdf https://www.bayesianscientific.org/wp-content/uploads/2022/10/DIA-BSWG-KOL-CPMP-21OCT2022_final.pdf
+wget --no-check-certificate -O 2022-11-neos_dia_bswg_nov22.pptx https://www.bayesianscientific.org/wp-content/uploads/2022/11/neos_dia_bswg_nov22.pptx
+wget --no-check-certificate -O 2022-12-Berry_CID.pdf https://www.bayesianscientific.org/wp-content/uploads/2022/12/Berry_CID.pdf
+wget --no-check-certificate -O 2023-03-cid-dia-bswg-webinar-feb-24.pptx https://www.bayesianscientific.org/wp-content/uploads/2023/03/cid-dia-bswg-webinar-feb-24.pptx
+wget --no-check-certificate -O 2023-04-dia23.pdf https://www.bayesianscientific.org/wp-content/uploads/2023/04/dia23.pdf
+wget --no-check-certificate -O 2023-04-SavilleTimeMachine_BSWG_2023.pdf https://www.bayesianscientific.org/wp-content/uploads/2023/04/SavilleTimeMachine_BSWG_2023.pdf
+wget --no-check-certificate -O 2023-05-MERIT-BSWG-YingYuan.pdf https://www.bayesianscientific.org/wp-content/uploads/2023/05/MERIT-BSWG-YingYuan.pdf
+wget --no-check-certificate -O 2023-07-BSWG_KOL_ClarkBray20230721_v1.pdf https://www.bayesianscientific.org/wp-content/uploads/2023/07/BSWG_KOL_ClarkBray20230721_v1.pdf
